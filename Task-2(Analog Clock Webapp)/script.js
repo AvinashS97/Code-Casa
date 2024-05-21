27797b64c2d27bbbb5ed5e49a879c5857b9f701d
AvinashS97 <128918711+AvinashS97@users.noreply.github.com>
2024-05-21 22:42:33 +0530

    Update time

--- Task-2(Analog Clock Webapp)/script.js
+++ Task-2(Analog Clock Webapp)/script.js
@@ -4,9 +4,9 @@ setInterval(() => {
     htime = d.getHours();
     mtime = d.getMinutes();
     stime = d.getSeconds();
-    hrotation = 30*htime + mtime/2;
-    mrotation = 6*mtime;
-    srotation = 6*stime;
+    hrotation = 30*htime + mtime/2; //rotation of hours neddle.
+    mrotation = 6*mtime; // rotation of Minutes neddle.
+    srotation = 6*stime; // rotation of Seconds neddle.
 
     hours.style.transform = `rotate(${hrotation}deg)`;
     minutes.style.transform = `rotate(${mrotation}deg)`;
